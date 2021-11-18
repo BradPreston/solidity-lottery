701a982b9691e60ccd36b779493b1ac4591bd59d
Brad Preston <bap5393@gmail.com>
2021-11-17 20:52:12 -0500

    finished section 3 of course

--- deploy.js
+++ deploy.js
@@ -1,12 +1,11 @@
 const HDWalletProvider = require('@truffle/hdwallet-provider');
 const Web3 = require('web3');
 const { interface, bytecode } = require('./compile');
+require('dotenv').config();
 
 const provider = new HDWalletProvider(
-  'REPLACE_WITH_YOUR_MNEMONIC',
-  // remember to change this to your own phrase!
-  'https://rinkeby.infura.io/v3/15c1d32581894b88a92d8d9e519e476c'
-  // remember to change this to your own endpoint!
+  process.env.RINKEBY_MNEMENIC,
+  process.env.NETWORK_URL
 );
 const web3 = new Web3(provider);
 
@@ -22,4 +21,5 @@ const deploy = async () => {
   console.log('Contract deployed to', result.options.address);
   provider.engine.stop();
 };
+
 deploy();
